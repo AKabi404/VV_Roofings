a7e4439d2cced0c303d0d3e3cff293319af422ff
AKabi404 <abineshjothi12345@gmail.com>
2026-02-12 20:50:51 +0530

    Implement hamburger menu in Header component
    Added a hamburger menu and logo to the header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,37 @@
+import { useState } from "react";
+import logo2 from "../assets/images/logo2.png";
+
+
 export default function Header() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <header className="header">
-      <div className="logo">VV<span>Roofing</span></div>
-      <nav>
-    <a href="#home">Home</a>
-    <a href="#about">About</a>
-    <a href="#products">Products</a>
-    <a href="#contact">Contact</a>
-    </nav>
-    <a href="tel:+919952611169" className="contact-btn">
-    +91 9952611169
-    </a>
-    </header>
+    <>
+      <header className="header">
+        {/* HAMBURGER MENU */}
+        <div className="menu-hamburger" onClick={() => setOpen(true)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        {/* LOGO + NAME */}
+        <div className="logo">
+          <img src={logo2} alt="VV Roofing Logo" className="logo-img" />
+          VV<span>Roofing</span>
+        </div>
+
+       
+      </header>
+
+      {open && <div className="overlay" onClick={() => setOpen(false)} />}
+
+      <aside className={`sidebar ${open ? "open" : ""}`}>
+        <a href="#home" onClick={() => setOpen(false)}>Home</a>
+        <a href="#about" onClick={() => setOpen(false)}>About</a>
+        <a href="#products" onClick={() => setOpen(false)}>Products</a>
+        <a href="#contact" onClick={() => setOpen(false)}>Contact</a>
+      </aside>
+    </>
   );
 }
